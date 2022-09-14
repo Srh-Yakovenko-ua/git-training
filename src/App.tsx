6dd301800fa7cf8e9653faab9df9e6147f089de5
Yakovenko Serhii <yakovenko.sergey.work@gmail.com>
2022-09-14 21:46:33 +0300

    developer/login branch commit 1

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@ function App() {
             <button>dev branch commit 1</button>
             <button>dev branch commit 2</button>
             <button>dev branch commit 3</button>
+            <button>developer/login branch commit 1</button>
         </div>
     );
 }
